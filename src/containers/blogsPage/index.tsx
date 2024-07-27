1e4c1a8f006118cbd06e16438940ffebb5fcd734
Nart <113383731+nartna123@users.noreply.github.com>
2024-07-27 19:24:31 +0300

    Update index.tsx

--- src/containers/blogsPage/index.tsx
+++ src/containers/blogsPage/index.tsx
@@ -16,7 +16,7 @@ const BLOGS_LIST: IBlog[] = [
     id: 0,
   },
   {
-    title: "Circassian could had gender distinguishing pronouns",
+    title: "Circassian could have had gender distinguishing pronouns",
     description: "",
     image: "",
     id: 1,
@@ -26,25 +26,32 @@ const BLOGS_LIST: IBlog[] = [
 export default function BlogsPageContainer() {
   const router = useRouter();
   return (
-    <div className="mx-auto mt-2 flex w-11/12 flex-col gap-2">
+    <div className="mx-auto mt-2 flex w-11/12 flex-col gap-4">
       <input
         className="mx-auto min-w-[400px] rounded-md border border-solid border-black p-4"
         placeholder="Search topic"
       />
-      {BLOGS_LIST.map((blog, idx) => {
-        return (
+      <div className="flex flex-row flex-wrap gap-4">
+        {BLOGS_LIST.map((blog, idx) => (
           <div
             onClick={() => router.push(`/blogs/${blog.id}`)}
-            key={blog.title}
-            className="flex flex-row items-center justify-between rounded-2xl bg-indigo-600/20 p-4 text-2xl shadow hover:cursor-pointer hover:bg-indigo-600/30 hover:underline"
+            key={blog.id}
+            className="w-[300px] hover:cursor-pointer lg:flex lg:max-w-full"
           >
-            <h2>
-              {idx}. {blog.title}
-            </h2>
-            <h2 className="text-3xl text-blue-600">Link</h2>
+            <div className="flex flex-col justify-between rounded-lg border border-gray-400 bg-indigo-600/10 p-4 leading-normal shadow-md hover:bg-indigo-600/20 hover:shadow-lg">
+              <div className="mb-8">
+                <div className="mb-2 text-xl font-bold text-gray-900">{blog.title}</div>
+                <p className="text-base text-gray-700">
+                  {blog.description || "No description available."}
+                </p>
+              </div>
+              <div className="flex items-center">
+                <div className="text-3xl text-blue-600 hover:underline">Enter</div>
+              </div>
+            </div>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
