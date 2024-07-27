bdf0ddb4b93e934b9b565cf53e6124ce0e04b8fb
Nart <113383731+nartna123@users.noreply.github.com>
2024-07-27 14:30:05 +0300

    .

--- src/containers/homePage/index.tsx
+++ src/containers/homePage/index.tsx
@@ -1,44 +1,79 @@
 import React from "react";
 import { USED_DICTS } from "~/constants/dicts";
 import { RANDOM_COLORS } from "~/constants/colors";
+import Image from "next/image";
 
 export default function HomePageContainer() {
   return (
-    <div className="container mx-auto my-8">
+    <div>
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export function Content() {
+  return (
+    <div className="mx-auto my-8 flex w-3/5 flex-col items-center justify-center gap-2 rounded-md bg-white shadow-2xl">
+      <Image src="/fav/icon-1042x1042.png" width={300} height={300} alt="logo" />
+      <p className="mb-4 text-center text-xl md:w-1/2">
+        Welcome to our comprehensive collection of Circassian language dictionaries! Our website is
+        dedicated to preserving and promoting the rich linguistic heritage of the Circassian people
+        by providing access to over 30 different dictionaries. These dictionaries encompass
+        translations between Circassian and several major languages: Russian, English, Arabic, and
+        Turkish.
+        <br />
+        <br />
+        Our collection includes dictionaries for both Western and Eastern Circassian, translating to
+        and from Turkish, English, Russian, and Arabic.
+        <br />
+        <br />
+        Whether you are a language enthusiast, a student, or a scholar, our extensive collection
+        offers valuable resources for learning and researching the Circassian language. Dive into
+        our website to explore the beauty and complexity of Circassian, a language spoken by the
+        Circassian people with a rich cultural history.
+      </p>
       <h1 className="mb-4 text-center text-3xl font-bold">
-        Dictionaries Used: {USED_DICTS.length}
+        List of dictionaries used: {USED_DICTS.length}
       </h1>
       <div className="mb-4 text-center">
         <p className="text-sm">Supporting Languages: Kbd, En, Ady, Ar, Tu & Ru</p>
       </div>
-      <div className="grid grid-cols-1 gap-4">
+      <div className="flex flex-wrap justify-center gap-2">
         {USED_DICTS.map((dict, index) => (
           <div
             key={index}
-            className="rounded-lg p-4 shadow-md"
+            className="w-full max-w-[300px] rounded-lg p-2 shadow-md md:w-1/2"
             style={{ backgroundColor: RANDOM_COLORS[index] }}
           >
-            <h2 className="mb-2 text-lg font-bold">{dict.title}</h2>
-            <p className="mb-2 text-sm">
+            <h2 className="mb-2 text-base font-bold leading-none">{dict.title}</h2>
+            <p className="mb-2 text-xs leading-none">
               <span className="font-semibold">From:</span> {dict.fromLang} -
               <span className="font-semibold"> To:</span> {dict.toLang}
             </p>
-            <p className="text-sm">
+            <p className="text-xs leading-none">
               <span className="font-semibold">Entries Count:</span> {dict.count}
             </p>
           </div>
         ))}
       </div>
-      <div className="mt-4 flex flex-col items-center justify-center gap-8 p-4">
-        <p className="whitespace-normal break-words text-center text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl">
-          You can content us at:
+    </div>
+  );
+}
+
+export function Footer() {
+  return (
+    <div className="w-full bg-[#eeeeff] shadow-xl">
+      <div className="mx-auto flex w-9/12 flex-col items-center justify-between gap-2 p-4 md:gap-8 lg:flex-row 2xl:gap-16">
+        <p className="flex flex-col gap-0 whitespace-normal break-words text-center text-base leading-none md:text-lg lg:gap-2 lg:text-xl xl:text-2xl 2xl:text-2xl 3xl:text-2xl 4xl:text-2xl">
+          You can contact us at:
           <br />
-          <span className="font-bold">learncircassian@gmail.com</span>
+          <span className="font-bold  leading-none">learncircassian@gmail.com</span>
         </p>
-        <p className="whitespace-normal break-words text-center text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl">
+        <p className="flex flex-col gap-0 whitespace-normal break-words text-center text-base leading-none md:text-lg lg:gap-2 lg:text-xl xl:text-2xl 2xl:text-2xl 3xl:text-2xl 4xl:text-2xl">
           You can get the dictionaries that we used at:
           <br />{" "}
-          <span className="font-bold">
+          <span className="font-bold  leading-none">
             https://github.com/bihoqo/circassian-dictionaries-collection
           </span>
         </p>
--- src/containers/layout/index.tsx
+++ src/containers/layout/index.tsx
@@ -1,6 +1,5 @@
 import "react-toastify/dist/ReactToastify.css";
 import React, { ReactNode } from "react";
-import { cn } from "~/utils/classNames";
 import Header from "~/containers/header";
 
 export interface LayoutProps {
@@ -11,18 +10,12 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <div data-test-id="main-header z-50">
-        <div className="z-50 w-full">
+        <div className="z-50 hidden w-full">
           <Header />
         </div>
       </div>
-      <div data-test-id="layout-container h-screen" className="flex">
-        <div className="relative flex w-full flex-row justify-center">
-          <section className={cn("flex flex-col w-full")}>
-            <div id="profile-bg" className="absolute inset-x-0 top-0 overflow-hidden" />
-            <div id="contribute-bg" className="absolute inset-x-0 top-0 " />
-            <div data-test-id="layout">{children}</div>
-          </section>
-        </div>
+      <div className="bg-image flex h-screen">
+        <div className="relative flex w-full flex-row justify-center">{children}</div>
       </div>
     </>
   );
