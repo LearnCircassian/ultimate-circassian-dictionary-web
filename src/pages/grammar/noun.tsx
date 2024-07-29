edae9e8c21154603da61a04c3cd1cd5e4536d5e3
ArturMarschenkulov <ArturMarschals@web.de>
2024-07-29 02:33:33 +0200

    add `TranslateText` and `HighlightText`

--- src/pages/grammar/noun.tsx
+++ src/pages/grammar/noun.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import H1 from "~/styled-components/h1";
-import CText from "~/styled-components/CText";
+import CText, { TranslateText, HighlightText } from "~/styled-components/CText";
 
 export default function NounPage() {
   return (
@@ -74,8 +74,11 @@ export default function NounPage() {
             <CText ipa="pχaːɕʼam pχabʁʷxar kʷabʒawə jəɕʼaːɕ">
               пхъащIэм пхъэбгъухэр куэбжэу ищIащ
             </CText>{" "}
-            : the carpenter made a gate out of planks (lit.{" "}
-            <span className="italic">the carpenter made planks like a gate</span>)
+            : <TranslateText>the carpenter made a gate out of planks</TranslateText> (lit.{" "}
+            <TranslateText>
+              <span className="italic">the carpenter made planks like a gate</span>
+            </TranslateText>
+            )
           </li>
         </ul>
 
@@ -131,11 +134,12 @@ export default function NounPage() {
 
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="ɕʼaːɮaxa qəzadaʔʷa">щIалэхэ, къызэдэIуэ</CText>: boys, listen to me
+            <CText ipa="ɕʼaːɮaxa qəzadaʔʷa">щIалэхэ, къызэдэIуэ</CText>:{" "}
+            <TranslateText>boys, listen to me</TranslateText>
           </li>
           <li>
-            <CText ipa="ɕʼaːɮaxa wənamtʃʼa fənajbɮaʁat">щIалэхэ, унэмкIэ фынеблагъэт</CText>: boys,
-            visit the house
+            <CText ipa="ɕʼaːɮaxa wənamtʃʼa fənajbɮaʁat">щIалэхэ, унэмкIэ фынеблагъэт</CText>:{" "}
+            <TranslateText>boys, visit the house</TranslateText>
           </li>
         </ul>
 
@@ -150,11 +154,11 @@ export default function NounPage() {
             <CText ipa="sawsrəqʷa naːrtxam maːfʼa qaːzarəxʷəjħaːr">
               сосрыкъуэ нартхэм мафIэ къазэрыхуихьар
             </CText>
-            : how Sosruko brought fire to the Narts
+            : <TranslateText>how Sosruko brought fire to the Narts</TranslateText>
           </li>
           <li>
-            <CText ipa="χʷaːʒa psəm qəzərəjtʃʼəʒaːr">хъуэжэ псым къызэрикIыжар</CText>: how Khaja
-            returned from the river
+            <CText ipa="χʷaːʒa psəm qəzərəjtʃʼəʒaːr">хъуэжэ псым къызэрикIыжар</CText>:{" "}
+            <TranslateText>how Khaja returned from the river</TranslateText>
           </li>
         </ul>
       </section>
@@ -170,36 +174,64 @@ export default function NounPage() {
         <p className="mb-4">It denotes the subject of an intransitive verb.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="χədʒabzər mabaːwa">хъыджэбзыр мэбауэ</CText>:{" "}
-            <span className="italic">the girl</span> breaths
+            <CText ipa="χədʒabzər mabaːwa">
+              <HighlightText>хъыджэбзыр</HighlightText> мэбауэ
+            </CText>
+            :{" "}
+            <TranslateText>
+              <HighlightText>the girl</HighlightText> breaths
+            </TranslateText>
           </li>
           <li>
-            <CText ipa="fəzər mazəm jawpɬ">фызыр мэзым йоплъ</CText>: :{" "}
-            <span className="italic">the woman</span> looks at the moon
+            <CText ipa="fəzər mazəm jawpɬ">
+              <HighlightText>фызыр</HighlightText> мэзым йоплъ
+            </CText>
+            : :{" "}
+            <TranslateText>
+              <HighlightText>the woman</HighlightText> looks at the moon
+            </TranslateText>
           </li>
         </ul>
 
         <p className="mb-4">It denotes the direct object of a transitive verb.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="sa qasɬəχʷaː ɬʼər qazʁʷataːɕ">сэ къэслъыхъуа лIыр къэзгъуэтащ</CText>: I
-            have found <span className="italic">the man</span> whom I was looking for
+            <CText ipa="sa qasɬəχʷaː ɬʼər qazʁʷataːɕ">
+              сэ къэслъыхъуа <HighlightText>лIыр</HighlightText> къэзгъуэтащ
+            </CText>
+            :{" "}
+            <TranslateText>
+              I have found <HighlightText>the man</HighlightText> whom I was looking for
+            </TranslateText>
           </li>
         </ul>
 
         <p className="mb-4">It denotes the nominal predicate in equative sentences.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="sa təkʷanəm zʁakʷʼaːr səj qʷaʃərɕ">сэ тыкуэным згъэкIуар си къуэшырщ</CText>
-            : whom I have sent to the store <span className="italic">is my brother</span>
+            <CText ipa="sa təkʷanəm zʁakʷʼaːr səj qʷaʃərɕ">
+              сэ тыкуэным згъэкIуар <HighlightText>си къуэшырщ</HighlightText>
+            </CText>
+            :{" "}
+            <TranslateText>
+              whom I have sent to the store <HighlightText>is my brother</HighlightText>
+            </TranslateText>
           </li>
           <li>
-            <CText ipa="sa χədʒabz daːxar səj nəsarɕ">сэ хъыджэбз дахэр си нысэрщ</CText>: this
-            pretty girl is my bride
+            <CText ipa="sa χədʒabz daːxar səj nəsarɕ">
+              сэ хъыджэбз дахэр <HighlightText>си нысэрщ</HighlightText>
+            </CText>
+            :
+            <TranslateText>
+              {" "}
+              this pretty girl <HighlightText>is my bride</HighlightText>
+            </TranslateText>
           </li>
           <li>
-            <CText ipa="səj qʷar ɕaːkʷʼarɕ">си къуэр щакIуэрщ</CText>: my brother{" "}
-            <span className="italic">is the hunter</span>
+            <CText ipa="səj qʷar ɕaːkʷʼarɕ">си къуэр щакIуэрщ</CText>:{" "}
+            <TranslateText>
+              my brother <HighlightText>is the hunter</HighlightText>
+            </TranslateText>
           </li>
         </ul>
       </section>
@@ -209,8 +241,13 @@ export default function NounPage() {
         <p className="mb-4">If used with transitive verbs it denotes the subject.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="ɬʼəm dəʁʷəʑ jəɬaʁʷaːɕ">лIым дыгъужь илъэгъуащ</CText>:{" "}
-            <span className="italic">the man</span> saw a wolf
+            <CText ipa="ɬʼəm dəʁʷəʑ jəɬaʁʷaːɕ">
+              <HighlightText>лIым</HighlightText> дыгъужь илъэгъуащ
+            </CText>
+            :{" "}
+            <TranslateText>
+              <HighlightText>the man</HighlightText> saw a wolf
+            </TranslateText>
           </li>
         </ul>
         <p className="mb-4">
@@ -219,41 +256,70 @@ export default function NounPage() {
         </p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="ħar ɕʼaːɮam japɬaːɕ">хьэр щIалэм еплъащ</CText>: the dog looked at{" "}
-            <span className="italic">the boy</span>
+            <CText ipa="ħar ɕʼaːɮam japɬaːɕ">
+              хьэр <HighlightText>щIалэм</HighlightText> еплъащ
+            </CText>
+            :{" "}
+            <TranslateText>
+              the dog looked at <HighlightText> the boy</HighlightText>
+            </TranslateText>
           </li>
         </ul>
         <ul className="mb-4 list-inside list-disc">
           <li>
             <CText ipa="ɕʼaːɮam məʔarəsa tħamaːdam jərətaːɕ">
-              щIалэм мыIэрысэ тхьэмадэм иритащ
+              щIалэм мыIэрысэ <HighlightText>тхьэмадэм</HighlightText> иритащ
             </CText>
-            : the boy gave <span className="italic">the Tkhamada</span> an applied
+            :{" "}
+            <TranslateText>
+              the boy gave <HighlightText>the Tkhamada</HighlightText> an applied
+            </TranslateText>
           </li>
         </ul>
         <p className="mb-4">It can also be used to create locative adverbs.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="wa qaːɮam pəjsmaw ptxaː">уэ къалэм письмо птха?</CText>: did you write the
-            letter <span className="italic">to the city</span>?
+            <CText ipa="wa qaːɮam pəjsmaw ptxaː">
+              уэ <HighlightText>къалэм</HighlightText> письмо птха?
+            </CText>
+            :{" "}
+            <TranslateText>
+              did you write the letter <HighlightText>to the city</HighlightText>?
+            </TranslateText>
           </li>
           <li>
-            <CText ipa="qʷaːʒam wəsʃanɕ">къуажэм усшэнщ</CText>: I&apos;ll lead you{" "}
-            <span className="italic">to the village</span>
+            <CText ipa="qʷaːʒam wəsʃanɕ">
+              <HighlightText>къуажэм</HighlightText> усшэнщ
+            </CText>
+            :{" "}
+            <TranslateText>
+              I&apos;ll lead you <HighlightText>to the village</HighlightText>
+            </TranslateText>
           </li>
         </ul>
         <p className="mb-4">It can also be used to create temporal adverbs.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="ʒaɕəm ħar mabaːna">жэщым хъэр мэбанэ</CText>: the dog barks
-            <span className="italic">at night</span>
+            <CText ipa="ʒaɕəm ħar mabaːna">
+              <HighlightText>жэщым</HighlightText> хъэр мэбанэ
+            </CText>
+            :{" "}
+            <TranslateText>
+              the dog barks <HighlightText>at night</HighlightText>
+            </TranslateText>
           </li>
         </ul>
         8
         <p className="mb-4">It is also used in possessive constructions to denote the possessor.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="fəzən jə nar">фызым и нэр</CText>: the woman&apos;s eyes
+            <CText ipa="fəzəm jə nar">
+              <HighlightText>фызым</HighlightText> и нэр
+            </CText>
+            :{" "}
+            <TranslateText>
+              <HighlightText>the woman</HighlightText>&apos;s eyes
+            </TranslateText>
           </li>
         </ul>
       </section>
@@ -263,25 +329,35 @@ export default function NounPage() {
         <p className="mb-4">It can denote an instrumental meaning.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="saːbəjr qaːɮamtʃʼa maːtxa">сабийр къалэмкIэ матхэ</CText>: the child writes{" "}
-            <span className="italic">with the pen</span>
+            <CText ipa="saːbəjr qaːɮamtʃʼa maːtxa">
+              сабийр <HighlightText>къалэмкIэ</HighlightText> матхэ
+            </CText>
+            :{" "}
+            <TranslateText>
+              the child writes <HighlightText>with the pen</HighlightText>
+            </TranslateText>
           </li>
           <li>
-            <CText
-              ipa="da tajndʒəzəm
-            qχʷəħɬaːtatʃʼa dəɬataːɕ"
-            >
-              дэ тенджызым кхъухьлъатэкIэ дылъэтащ
+            <CText ipa="da tajndʒəzəm qχʷəħɬaːtatʃʼa dəɬataːɕ">
+              дэ тенджызым <HighlightText>кхъухьлъатэкIэ</HighlightText> дылъэтащ
             </CText>
-            : we flew to the sea <span className="italic">with a plane</span>
+            :{" "}
+            <TranslateText>
+              we flew to the sea <HighlightText>with a plane</HighlightText>
+            </TranslateText>
           </li>
         </ul>
 
         <p className="mb-4">It can denote the direction.</p>
         <ul className="mb-4 list-inside list-disc">
           <li>
-            <CText ipa="bɣəm tajt wənamtʃʼa maːpɬa">бгым тет унэмкIэ маплъэ</CText>: he looks
-            <span className="italic"> towards the house</span> standing on a hill
+            <CText ipa="bɣəm tajt wənamtʃʼa maːpɬa">
+              бгым тет <HighlightText>унэмкIэ</HighlightText> маплъэ
+            </CText>
+            :{" "}
+            <TranslateText>
+              he looks <HighlightText> towards the house</HighlightText> standing on a hill
+            </TranslateText>
           </li>
         </ul>
       </section>
--- src/styled-components/CText.tsx
+++ src/styled-components/CText.tsx
@@ -1,31 +1,20 @@
 import React, { ReactNode, useState } from "react";
 import { cn } from "~/utils/classNames";
 
-const HoverableIPA = ({ word, ipa }: { word: string; ipa: string }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div className="relative">
-      <span
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className="cursor-pointer"
-      >
-        {word}
-      </span>
-      {isHovered && (
-        <div className="absolute left-0 rounded border bg-white p-2 shadow-lg">{ipa}</div>
-      )}
-    </div>
-  );
-};
-
 function HoverBox({ children }: { children: ReactNode }) {
   return (
     <div className="absolute bottom-full left-0 mb-2 rounded border bg-white p-2 shadow-lg">
       {children}
     </div>
   );
+}
+
+export function TranslateText({ children }: { children: ReactNode }) {
+  return <span className="text-black-500">{children}</span>;
+}
+
+export function HighlightText({ children }: { children: ReactNode }) {
+  return <span className="font-bold text-blue-500">{children}</span>;
 }
 
 export default function CText({
