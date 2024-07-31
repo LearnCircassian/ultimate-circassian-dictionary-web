0041a6498d841a47cf36fc28be7ff227c2f73688
ArturMarschenkulov <ArturMarschals@web.de>
2024-07-31 16:53:38 +0200

    add beginning of verb chapter

--- src/pages/grammar/morphology.tsx
+++ src/pages/grammar/morphology.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GrammarBookContainer from "~/styled-components/GrammarBookContainer";
+import { GrammarBookContainer } from "~/styled-components";
 
 export default function MorphologyPage() {
   return (
