3635ee9cc3945a4c820bc9061261c5ed285675a8
Adam Sagas <59337744+bihoqo@users.noreply.github.com>
2024-08-01 14:59:58 +0300

    Ashmouz Hilmi's Turkish Dictionary (#25)
    * Disappear header on mobile
    * Fix dictionary page width & add hilmi counter

--- src/pages/dictionary/[word].tsx
+++ src/pages/dictionary/[word].tsx
@@ -9,8 +9,10 @@ import DefinitionsContainer from "~/containers/wordPage/definitionsContainer";
 import WordPageFooter from "~/containers/wordPage/wordPageFooter";
 import ContainerDiv from "~/components/containerDiv";
 import SearchContainer from "~/containers/header/searchContainer";
+import useWindowSize from "~/hooks/useWindowDimensions";
 
 export default function WordPage() {
+  const { width } = useWindowSize();
   const params = useParams<{ word: string }>();
   const wordSpelling = safeWordToRegularWord(params?.word || "");
 
@@ -22,16 +24,20 @@ export default function WordPage() {
       <main className="size-full">
         <ContainerDiv>
           <div className="flex w-full flex-grow flex-col gap-4">
-            <h1 className="w-full text-center text-5xl font-semibold">Circassian Dictionary</h1>
+            <h1
+              className={cn("w-full text-center text-5xl font-semibold", { hidden: width < 400 })}
+            >
+              Circassian Dictionary
+            </h1>
             <SearchContainer showOnMobile={false} />
           </div>
           <div
             className={cn(
-              "mx-auto my-20 flex px-2 sm:px-4 flex-[1] flex-row",
+              "mx-auto my-20 flex px-2 sm:px-4 flex-[1] flex-row w-full",
               "gap-2 sm:gap-4 md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-12 3xl:gap-16",
             )}
           >
-            <div className="mt-4 hidden md:flex lg:flex-[2] xl:flex-[2] 2xl:flex-[2] 3xl:flex-[1]">
+            <div className="mt-4 hidden md:flex lg:flex-[2] xl:flex-[2] 2xl:flex-[2] 3xl:flex-[2]">
               <WordHistoryContainer />
             </div>
             <div className="flex w-full flex-[7]">
